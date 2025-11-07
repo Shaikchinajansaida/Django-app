422667d46396322d36e6c7342de98f53db742a1a
Shaikchinajansaida <jansaida653@gmail.com>
2025-11-07 11:26:23 +0530

    intial step

---
+++ myProject/basic/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+# Create your views here.
+def sample (request):
+    return HttpResponse("Hello")
+def sampleinfo (request):
+    data ={"result":[1,2,3]}
+    return JsonResponse(data)
+
+
